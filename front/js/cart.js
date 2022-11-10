08df856e54b8c48c26ee75867d1cb8558346591f
rpaillargue <remi.paillargue@gmail.com>
2022-11-10 09:59:33 +0100

    bouton supprimer

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,18 @@
+async function getProducts() {
+  return fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (products) {
+      return products;
+    });
+}
+
+const produits = await getProducts();
+console.log(produits);
+
+const cardItems = document.getElementById("cart__items");
+
 let productFromLocalStorage = JSON.parse(localStorage.getItem("panier"));
 console.log(productFromLocalStorage);
 
@@ -10,7 +25,10 @@ if (!productFromLocalStorage) {
   sectionCart.style.display = "none";
 } else {
   for (let i = 0; i < productFromLocalStorage.length; i++) {
-    console.log(productFromLocalStorage[i]);
+    const produit = produits.find(
+      (item) => item._id === productFromLocalStorage[i].id
+    );
+    console.log(produit);
     // Création et insertion de l'élement article
     const cartArticle = document.createElement("article");
     cartArticle.classList.add("cart__item");
@@ -22,31 +40,31 @@ if (!productFromLocalStorage) {
     const cartDivImg = document.createElement("div");
     cartDivImg.classList.add("cart__item__img");
     cartArticle.appendChild(cartDivImg);
-    console.log(cartDivImg);
+    //console.log(cartDivImg);
 
     // Création et insertion de l'image
     const cartImg = document.createElement("img");
-    // Appel API à faire
-    cartImg.src = productFromLocalStorage[i].imageURL;
-    cartImg.alt = productFromLocalStorage[i].altTxt;
-    console.log(cartImg);
+    cartImg.src = produit.imageUrl;
+    cartImg.alt = produit.altTxt;
+    cartDivImg.appendChild(cartImg);
+    //console.log(cartImg);
 
     // Création et insertion de la div content
     const cartDivContent = document.createElement("div");
     cartDivContent.classList.add("cart__item__content");
     cartArticle.appendChild(cartDivContent);
-    console.log(cartDivContent);
+    //console.log(cartDivContent);
 
     // Création et insertion de la div description
     const cartDivDescription = document.createElement("div");
     cartDivDescription.classList.add("cart__item__content__description");
     cartDivContent.appendChild(cartDivDescription);
     cartArticle.appendChild(cartDivContent);
-    console.log(cartDivDescription);
+    //console.log(cartDivDescription);
 
     //Création et insertion de l'élément titre "h2"
     const cartTitleProduct = document.createElement("h2");
-    cartTitleProduct.innerHTML = productFromLocalStorage[i].name;
+    cartTitleProduct.innerHTML = produit.name;
     cartDivDescription.appendChild(cartTitleProduct);
     cartDivContent.appendChild(cartDivDescription);
     cartArticle.appendChild(cartDivContent);
@@ -54,16 +72,15 @@ if (!productFromLocalStorage) {
 
     // Création et insertion de l'élément couleur "p"
     const cartColorProduct = document.createElement("p");
-    cartColorProduct.innerHTML = productFromLocalStorage[i].colors;
+    cartColorProduct.innerHTML = productFromLocalStorage[i].color;
     cartDivDescription.appendChild(cartColorProduct);
     cartDivContent.appendChild(cartDivDescription);
     cartArticle.appendChild(cartDivContent);
     //console.log(cartColorProduct)
 
     // Création et insertion de l'élément prix "p"
-    // Appel API à faire
     const cartPriceProduct = document.createElement("p");
-    cartPriceProduct.innerHTML = productFromLocalStorage[i].price;
+    cartPriceProduct.innerHTML = produit.price + " €";
     cartDivDescription.appendChild(cartPriceProduct);
     cartDivContent.appendChild(cartDivDescription);
     cartArticle.appendChild(cartDivContent);
@@ -74,7 +91,7 @@ if (!productFromLocalStorage) {
     cartContentSettings.classList.add("cart__item__content__settings");
     cartDivContent.appendChild(cartContentSettings);
     cartArticle.appendChild(cartDivContent);
-    console.log(cartContentSettings);
+    //console.log(cartContentSettings);
 
     // Création et insertion de l'élément settings quantitty "div"
     const cartSettingsQuantity = document.createElement("div");
@@ -87,8 +104,6 @@ if (!productFromLocalStorage) {
     //console.log(cartSettingsQuantity)
 
     // Création et insertion de l'élément quantité "p"
-    //<p>Qté : </p>
-    //<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42"></input>
     const cartQuantityProduct = document.createElement("p");
     cartQuantityProduct.innerHTML = "Qté : ";
     cartSettingsQuantity.appendChild(cartQuantityProduct);
@@ -101,19 +116,47 @@ if (!productFromLocalStorage) {
     cartInputQuantity.setAttribute("name", "itemQuantity");
     cartInputQuantity.setAttribute("min", "1");
     cartInputQuantity.setAttribute("max", "100");
-    //cartInputQuantity.value = productFromLocalStorage.quantity;
+    cartInputQuantity.setAttribute(
+      "value",
+      productFromLocalStorage[i].quantity
+    );
     cartSettingsQuantity.appendChild(cartInputQuantity);
     //console.log(cartInputQuantity);
 
-    // Création et insertion de l'élement delete "div" et "p"
+    // Création et insertion de l'élement delete "div"
     const cartSettingsDelete = document.createElement("div");
     cartSettingsDelete.classList.add("cart__item__content__settings__delete");
     cartContentSettings.appendChild(cartSettingsDelete);
     //console.log(cartSettingsDelete);
+
+    // Création et insertion de l'élément delete "p"
     const cartDelete = document.createElement("p");
     cartDelete.classList.add("deleteItem");
     cartDelete.innerHTML = "Supprimer";
     cartSettingsDelete.appendChild(cartDelete);
+    cartSettingsDelete.addEventListener("click", () => {
+      console.log("DELETEEEE");
+      console.log(productFromLocalStorage);
+      // Quand on clique sur le bouton Supprimer
+      const produitsRestants = productFromLocalStorage.filter(
+        (item) =>
+          item.id !== productFromLocalStorage[i].id ||
+          item.color !== productFromLocalStorage[i].color.value
+      );
+      console.log(produitsRestants);
+      localStorage.setItem("panier", JSON.stringify(productFromLocalStorage));
+      // Si aucun produit dans le local storage on affiche le panier vide
+      if (produitsRestants.length === 0) {
+        localStorage.clear();
+      }
+      // Et on raffraichit la page
+      location.reload();
+    });
+    // Ajout au clic de quantité
+    const addQuantity = document.getElementsByClassName("itemQuantity");
+    console.log(addQuantity);
+
+    cardItems.appendChild(cartArticle);
     //console.log(cartDelete);
   }
 }
